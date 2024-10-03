13e3d648f10d577d311a2073352acf9e651724eb
Mauricio <mcampo26@gmail.com>
2024-10-02 21:46:45 -0300

    instalo websoket

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
 import cors from 'cors';
-import routerPagos from './src/Routes/Pagos/index.js';
-import routerProductos from './src/Routes/Productos/index.js';
-import morgan from 'morgan';
-import { dbConnect } from './src/database/config.js';
 
 const app = express();
-const PORT = process.env.PORT || 8080;
-
-// Middleware para parsear JSON
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Configuración de CORS
-app.use(cors({
-  origin: ['https://thepoint.netlify.app'], // Permite solicitudes desde el dominio de tu frontend
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'], // Permite estos encabezados
-  credentials: true, // Permitir envío de cookies o autenticación basada en tokens
-}));
-
-// Middleware para manejar solicitudes preflight (OPTIONS)
-app.options('*', cors());
-
-app.use(morgan('dev'));
-
-// Montar rutas
-app.use('/Pagos', routerPagos);
-app.use('/Productos', routerProductos);
-
-// Iniciar servidor
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+const server = http.createServer(app); // Crear servidor HTTP
+const io = new Server(server, {
+  cors: {
+    origin: [ 'https://thepoint.netlify.app/'], // Reemplaza con la URL de tu frontend
+    methods: ['GET', 'POST'],
+  },
 });
 
-// Conectar a la base de datos
-dbConnect()
-  .then(() => {
-    console.log('Estoy listo y conectado a la base de datos');
-  })
-  .catch(error => {
-    console.error('Error al conectar con la base de datos:', error);
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('Cliente conectado');
+
+  // Puedes enviar eventos desde el backend al frontend
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado');
   });
+});
+
+// Inicia el servidor
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
