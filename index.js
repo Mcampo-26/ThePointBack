a019cae9e7c739dfd81a86b9a98e8a18bfefda37
Mauricio <mcampo26@gmail.com>
2024-10-02 21:56:59 -0300

    corrigo idex con web sockets

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
 import cors from 'cors';
+import morgan from 'morgan';
+import http from 'http'; // Importar el módulo http
+import { Server } from 'socket.io'; // Importar socket.io
+import routerPagos from './src/Routes/Pagos/index.js';
+import routerProductos from './src/Routes/Productos/index.js';
+import { dbConnect } from './src/database/config.js';
 
 const app = express();
 const server = http.createServer(app); // Crear servidor HTTP
 const io = new Server(server, {
   cors: {
-    origin: [ 'https://thepoint.netlify.app'], // Reemplaza con la URL de tu frontend
+    origin: ['https://thepoint.netlify.app'], // Reemplaza con la URL de tu frontend
     methods: ['GET', 'POST'],
   },
 });
+const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+// Middleware para parsear JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Configuración de CORS
+app.use(cors({
+  origin: ['https://thepoint.netlify.app'], // Permite solicitudes desde el dominio de tu frontend
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'], // Permite estos encabezados
+  credentials: true, // Permitir envío de cookies o autenticación basada en tokens
+}));
+
+// Middleware para manejar solicitudes preflight (OPTIONS)
+app.options('*', cors());
+
+// Configuración de Socket.io
 io.on('connection', (socket) => {
   console.log('Cliente conectado');
 
@@ -23,8 +42,22 @@ io.on('connection', (socket) => {
   });
 });
 
-// Inicia el servidor
-const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
+app.use(morgan('dev'));
+
+// Montar rutas
+app.use('/Pagos', routerPagos);
+app.use('/Productos', routerProductos);
+
+// Iniciar servidor
+server.listen(PORT, () => { // Usar server.listen en lugar de app.listen
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
+
+// Conectar a la base de datos
+dbConnect()
+  .then(() => {
+    console.log('Estoy listo y conectado a la base de datos');
+  })
+  .catch(error => {
+    console.error('Error al conectar con la base de datos:', error);
+  });
