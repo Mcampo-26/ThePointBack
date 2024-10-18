62428031c034e336f226cc9bc9e6a9b23131827b
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2024-10-18 12:20:21 -0300

    Update index.js

--- src/Routes/Pagos/index.js
+++ src/Routes/Pagos/index.js
@@ -3,14 +3,22 @@ import {
   createPaymentLink,
   savePaymentDetails,
   receiveWebhook,
-} from "../../controllers/pagoscontrollers/index.js";
+  createModoCheckout
 
+} from "../../controllers/pagoscontrollers/index.js";
+//import validateQRBeforePayment from "../../middleware/validateQRBeforePayment.js"; // Importa el middleware de validación de QR
 
 const router = express.Router();
 
+// Ruta para crear el enlace de pago con la validación del QR antes del pago
 router.post('/create_payment_link', createPaymentLink);
+router.post('/create_modo', createModoCheckout);
+// Ruta para guardar los detalles del pago
 router.post('/save_payment_details', savePaymentDetails);
-router.post('/webhook', receiveWebhook);  // Ajusta la ruta para el webhook
 
+
+
+// Ruta para recibir los webhooks de Mercado Pago
+router.post('/webhook', receiveWebhook);
 
 export default router;
