af9433303d9b6696a08ad4e56bbf756ea50ec41c
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2024-10-18 12:15:57 -0300

    Update index.js

--- src/controllers/pagoscontrollers/index.js
+++ src/controllers/pagoscontrollers/index.js
@@ -240,3 +240,52 @@ export const receiveWebhook = async (req, res) => {
   }
 };
 
+
+
+
+import  {MODO_TOKEN} from '../Modo/index.js'
+
+export const createModoCheckout = async (req, res) => {
+    console.log("Recibiendo solicitud para crear checkout de MODO"); // L
+    const { price } = req.body;
+  
+    try {
+        console.log("Datos recibidos:", price);
+      const response = await axios.post("https://api.modo.com.ar/payments", {
+        amount: price,
+        currency: "ARS", // Moneda en formato ISO 4217
+        description: "Compra en tienda",
+        external_reference: "ID_UNICO_DE_TRANSACCION" // Puedes generar un ID único para cada transacción
+    }, {
+        headers: {
+                 'Authorization': `Bearer ${MODO_TOKEN}`
+        }
+    });
+  
+      const {  qr_url, deeplink, external_reference} = response.data;
+      console.log("Respuesta de MODO recibida:", qr_url, deeplink); //
+      res.json({  qr_url, deeplink, external_reference});
+    } catch (error) {
+      res.status(500).json({ message: "Error creando la intención de pago" });
+    }
+  };
+
+// Controlador para manejar el webhook de MODO (sin almacenar datos)
+export const receiveModoWebhook = async (req, res) => {
+    try {
+      const { paymentId, status, amount, date } = req.body;
+  
+      // Simplemente imprime los datos recibidos para verificarlos
+      console.log("Webhook de MODO recibido:");
+      console.log("ID del pago:", paymentId);
+      console.log("Estado del pago:", status);
+      console.log("Monto:", amount);
+      console.log("Fecha de la transacción:", date);
+  
+      // Responder al webhook de MODO
+      res.status(200).json({ message: "Webhook recibido y procesado correctamente" });
+    } catch (error) {
+      console.error("Error al procesar el webhook de MODO:", error);
+      res.status(500).json({ message: "Error al procesar el webhook" });
+    }
+  };
