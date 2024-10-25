d60950108cecd9d3d9aa4378296cfed76c2952b9
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2024-10-25 08:25:58 -0300

    Update index.js

--- src/controllers/pagoscontrollers/index.js
+++ src/controllers/pagoscontrollers/index.js
@@ -228,7 +228,6 @@ export const createModoCheckout = async (req, res) => {
       storeId: storeId, // Aquí se pasa el storeId correcto
       externalIntentionId: externalIntentionId,
       expirationDate: expirationDate,
-      socketId: socketId,
      
       message: 'Este mensaje se traslada desde la intención de pago hasta el webhook',
     }, {
