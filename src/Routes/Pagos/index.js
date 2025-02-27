cf29c100781ca09133c1a140fc63297a20fef897
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2025-02-27 14:44:34 -0300

    Update index.js

--- src/Routes/Pagos/index.js
+++ src/Routes/Pagos/index.js
@@ -1,8 +1,7 @@
 import express from "express";
 import {
   createInteroperableQR, // Importa el nuevo controlador
-  //createPaymentLink,
-  savePaymentDetails,
+
   receiveWebhook,
 } from "../../controllers/pagoscontrollers/index.js";
 
@@ -15,7 +14,7 @@ router.put('/create_interoperable_qr', createInteroperableQR);
 //router.post('/create_payment_link', createPaymentLink);
 
 // Ruta para guardar los detalles del pago
-router.post('/save_payment_details', savePaymentDetails);
+
 
 // Ruta para recibir los webhooks de Mercado Pago
 router.post('/webhook', receiveWebhook);
